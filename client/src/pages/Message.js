8d02bd85ef299a58e15feab687f2e4b9f9503efa
fangweb <fangwebdev@gmail.com>
2019-04-24 01:48:35 -0700

    Initialize pages

---
+++ client/src/pages/Message.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import '../assets/css/message.css';
+
+class Message extends Component {
+  render() {
+    // props.match.params
+    return <div>Message</div>;
+  }
+}
+
+export default Message;
