2556064958c38c1b21e24f6710d5a58f6452b14a
fangweb <fangwebdev@gmail.com>
2019-05-02 16:33:33 -0700

    Add TestArea header link

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../assets/css/header.css';
 
 function Header() {
   return (
     <header>
       <ul className="menu">
+        <li>
+          <Link to="/testarea">
+            <i className="fas fa-vial"></i>
+            <span>Test Area</span>
+          </Link>
+        </li>
         <li>
           <a href="/placeholder">
             <i className="fas fa-door-closed" />
