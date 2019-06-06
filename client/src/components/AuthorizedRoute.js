e6f864ec36dd97b75808260cf43bf01a7d8c19e2
fangweb <fangwebdev@gmail.com>
2019-06-06 09:00:03 -0700

    Remove old authorization component

--- client/src/components/AuthorizedRoute.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
-import { SignInPath } from '../const'; 
-
-export default AuthorizedRoute = ({ isAuthenticated, ...props }) => 
-  isAuthenticated 
-  ? <Route {...props}/> 
-  : <Redirect to={SignInPath} />;
