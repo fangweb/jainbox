67a8644b1cbf789234f92a416134dd2e8b1b5e10
fangweb <fangwebdev@gmail.com>
2019-09-02 20:02:25 -0700

    Refactor headers

--- client/src/services/apiService.js
+++ client/src/services/apiService.js
@@ -1,12 +1,10 @@
 import { ApiConfig } from '../const';
-import { ServiceContainer } from '.';
 
 export class ApiService {
   constructor() {
-    const SessionService = ServiceContainer().session();
     this.headers = {
       Accept: ApiConfig.HeaderAcceptValue,
-      Authorization: `${ApiConfig.AuthScheme} ${SessionService.getJwt()}`,
+      Authorization: `${ApiConfig.AuthScheme}`,
       'Content-Type': ApiConfig.HeaderContentTypeValue
     };
   }
