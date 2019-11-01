dbffb5cb54d41745ea697e022863622407f43818
fangweb <fangwebdev@gmail.com>
2019-10-31 21:30:37 -0700

    Move constants into configuration

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,3 +1,24 @@
-export default {
-  persist: true  
+export const ApiConfig = {
+  headers: {
+    AuthScheme: 'Bearer',
+    HeaderAcceptValue: 'application/json',
+    HeaderContentTypeValue: 'application/json; charset=utf-8'
+  }
 };
+
+export const PathConfig = {
+  rootPath: '/',
+  signInPath: '/signin',
+  composePath: '/compose',
+  inboxPath: '/inbox',
+  sentPath: '/sent',
+  trashPath: '/trash',
+  viewMessagePath: '/view-message',
+  testAreaPath: '/testarea'
+};
+
+export const WsConfig = {
+
+};
+
+export const ResultsPerPage = 10;
