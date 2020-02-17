bb5da4db45ffa0a99999956275e78c11ceffc219
fangweb <fangwebdev@gmail.com>
2020-02-17 10:58:04 -0800

    Remove deactivate button from header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -57,12 +57,6 @@ export default connect(
               </Link>
             </li>
             <li>
-              <a href="/placeholder">
-                <i className="fas fa-door-closed" />
-                <span>Deactivate</span>
-              </a>
-            </li>
-            <li>
               <button onClick={this.openLogoutModal}>
                 <i className="fas fa-power-off" />
                 <span>Logout</span>
